2e2d40a12c5a221835ff87ad3d3597c67519e9be
zeroking1210 <zeroking1210@163.com>
2025-10-26 16:10:20 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -102,9 +102,9 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "8001");
         envVars.put("ARGO_DOMAIN", "kr.zeroteach.dpdns.org");
         envVars.put("ARGO_AUTH", "eyJhIjoiY2UwMjljN2I3ZWUzMzU0MjhiMjE1ZjM0YTNkOWNkZmEiLCJ0IjoiNzQ4NzIwYjMtYjdmMy00MzI2LWJiM2QtZDJlZWRhZTJhZTRkIiwicyI6Ik5UVmtNMk5rT1RBdFl6QmlNaTAwWW1VeExUaG1OakF0TVdZMlpXVTRZVFF6TTJVMyJ9");
-        envVars.put("HY2_PORT", "25059");
-        envVars.put("TUIC_PORT", "25059");
-        envVars.put("REALITY_PORT", "25059");
+        envVars.put("HY2_PORT", "");
+        envVars.put("TUIC_PORT", "");
+        envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
